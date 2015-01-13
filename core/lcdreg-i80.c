62ac7e0fdf7ecc6ce1b960568707f631f669f8b7
notro <notro@tronnes.org>
2015-01-13 17:45:18 +0100

    lcdreg-i80: add 16 -> 8 width support

--- core/lcdreg-i80.c
+++ core/lcdreg-i80.c
@@ -16,9 +16,7 @@ struct lcdreg_i80 {
 	struct lcdreg reg;
 	unsigned buswidth;
 	void *regnr_buf;
-	/* gpios */
-struct gpio_desc *cs;
-	struct gpio_desc *dc;
+	void *buffer;
 	struct gpio_desc *reset;
 };
 
@@ -27,34 +25,28 @@ static inline struct lcdreg_i80 *to_lcdreg_i80(struct lcdreg *reg)
 	return reg ? container_of(reg, struct lcdreg_i80, reg) : NULL;
 }
 
-
-/*
-
-NO conversion yet, only 8->8 and 16->16.
-
-*/
-
-
-/* FIXME: emulation */
 static int lcdreg_i80_write_regnr(struct lcdreg *reg, unsigned regnr)
 {
 	struct lcdreg_i80 *i80lcd = to_lcdreg_i80(reg);
+	struct i80_device *i80 = i80lcd->i80;
+	struct i80_master *master = i80->master;
 	void *buf = i80lcd->regnr_buf;
 	unsigned len = lcdreg_bytes_per_word(reg->def_width);
-//	int ret = -ENOSYS;
-
-//	if (reg->def_width == master->data_width) {
 
 	switch (len) {
-	case 4:
-		*(u32 *)buf = regnr;
-		break;
 	case 2:
-		*(u16 *)buf = regnr;
+		if (master->data_width == 16)
+			*(u16 *)buf = regnr;
+		else if (master->data_width == 8)
+			*(u16 *)buf = cpu_to_be16(regnr);
+		else
+			return -EINVAL;
 		break;
-	default:
+	case 1:
 		*(u8 *)buf = regnr;
 		break;
+	default:
+		return -EINVAL;
 	}
 
 	return i80_write(i80lcd->i80, 0, buf, len);
@@ -73,9 +65,49 @@ static int lcdreg_i80_write(struct lcdreg *reg, unsigned regnr, struct lcdreg_tr
 	if (ret || !transfer || !transfer->count)
 		goto done;
 
-/* FIXME: emulation */
-	if (transfer->width == master->data_width)
+	if (transfer->width == master->data_width) {
 		ret = i80_write(i80, transfer->index, transfer->buf, transfer->count * lcdreg_bytes_per_word(transfer->width));
+		goto done;
+	}
+
+#ifdef __BIG_ENDIAN
+	/* on big endian the byte order matches */
+	if (master->data_width == 8 &&
+	    (transfer->width == 16 || transfer->width ==  24)) {
+		ret = i80_write(i80, transfer->index, transfer->buf, transfer->count * lcdreg_bytes_per_word(transfer->width));
+		goto done;
+	}
+#endif
+
+	if (!i80lcd->buffer) {
+		i80lcd->buffer = devm_kzalloc(&i80->dev, PAGE_SIZE, GFP_KERNEL);
+		if (!i80lcd->buffer) {
+			ret = -ENOMEM;
+			goto done;
+		}
+	}
+
+	ret = -EINVAL;
+	if (transfer->width == 16 && master->data_width == 8) {
+		u16 *data16 = transfer->buf;
+		u16 *buffer16 = i80lcd->buffer;
+		unsigned remain = transfer->count;
+		unsigned tx_array_size = PAGE_SIZE / 2;
+		unsigned to_copy;
+		int i;
+
+		while (remain) {
+			to_copy = remain > tx_array_size ? tx_array_size : remain;
+			remain -= to_copy;
+			dev_dbg(reg->dev, "    to_copy=%zu, remain=%zu\n",
+						to_copy, remain);
+			for (i = 0; i < to_copy; i++)
+				buffer16[i] = cpu_to_be16(*data16++);
+			ret = i80_write(i80, transfer->index, buffer16, to_copy * 2);
+			if (ret < 0)
+				goto done;
+		}
+	}
 
 done:
 	i80_close(i80);
@@ -103,9 +135,49 @@ static int lcdreg_i80_read(struct lcdreg *reg, unsigned regnr, struct lcdreg_tra
 	if (ret)
 		goto done;
 
-/* FIXME: emulation */
-	if (transfer->width == master->data_width)
+	if (transfer->width == master->data_width) {
 		ret = i80_read(i80, transfer->index, transfer->buf, transfer->count * lcdreg_bytes_per_word(transfer->width));
+		goto done;
+	}
+
+#ifdef __BIG_ENDIAN
+	/* on big endian the byte order matches */
+	if (master->data_width == 8 && transfer->width == 16) {
+		ret = i80_write(i80, transfer->index, transfer->buf, transfer->count * lcdreg_bytes_per_word(transfer->width));
+		goto done;
+	}
+#endif
+
+	if (!i80lcd->buffer) {
+		i80lcd->buffer = devm_kzalloc(&i80->dev, PAGE_SIZE, GFP_KERNEL);
+		if (!i80lcd->buffer) {
+			ret = -ENOMEM;
+			goto done;
+		}
+	}
+
+	ret = -EINVAL;
+	if (transfer->width == 16 && master->data_width == 8) {
+		u16 *data16 = transfer->buf;
+		u16 *rxbuf16 = i80lcd->buffer;
+		unsigned remain = transfer->count;
+		unsigned rx_array_size = PAGE_SIZE / 2;
+		unsigned to_copy;
+		int i;
+
+		while (remain) {
+			to_copy = remain > rx_array_size ? rx_array_size : remain;
+			remain -= to_copy;
+			dev_dbg(reg->dev, "    to_copy=%zu, remain=%zu\n",
+						to_copy, remain);
+			ret = i80_read(i80, transfer->index, rxbuf16, to_copy * 2);
+			if (ret < 0)
+				goto done;
+
+			for (i = 0; i < to_copy; i++)
+				*data16++ = be16_to_cpu(rxbuf16[i]);
+		}
+	}
 
 done:
 	i80_close(i80);
@@ -152,7 +224,7 @@ struct lcdreg *devm_lcdreg_i80_init(struct i80_device *i80,
 	if (!i80lcd)
 		return ERR_PTR(-ENOMEM);
 
-	i80lcd->regnr_buf = devm_kzalloc(&i80->dev, sizeof(u32), GFP_KERNEL);
+	i80lcd->regnr_buf = devm_kzalloc(&i80->dev, sizeof(u16), GFP_KERNEL);
 	if (!i80lcd->regnr_buf)
 		return ERR_PTR(-ENOMEM);
 
